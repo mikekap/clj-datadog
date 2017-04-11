d600c679299b192edb2e5f74f8dadcd327439999
Andrey Neverov <neverov.and@gmail.com>
2017-04-11 12:22:46 +0300

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-datadog "3.0.0"
+(defproject clj-datadog "3.0.1"
   :description "Clojure client for DataDog service via statsd protocol"
   :url "https://github.com/truckerpathteam/clj-datadog"
 
